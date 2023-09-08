138ddb4712364e18e09c3869acb237e35e2d26c8
Nurs <nurrs.serkul@gmail.com>
2023-09-08 18:58:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = reqiure("express")
+const mongoose = require("mongoose")
+const app = express()
+app.use(express.json())
+
+const startServer = () => {
+    app.listen(8000)
+}
+
+startServer()
